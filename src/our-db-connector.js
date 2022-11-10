642a1b110cbdb66ffb53de9d036223f29d9dc130
17Tralfagar <rybckin.rae2013@yandex.ru>
2022-11-10 15:29:16 +0700

    Fix: 'console.log"

--- src/our-db-connector.js
+++ src/our-db-connector.js
@@ -2,10 +2,8 @@ const fastifyPlugin = require("fastify-plugin");
 const fastifyMongo = require("@fastify/mongodb");
 
 async function dbConnector(fastify, options) {
-  console.log(0);
   fastify.register(fastifyMongo, {
     url: "mongodb://localhost:27017/test_database",
   });
-  console.log(1);
 }
 module.exports = fastifyPlugin(dbConnector);
--- src/server.js
+++ src/server.js
@@ -5,11 +5,8 @@ const firstRoute = require("./out-first-route");
 const fastify = Fastify({
   logger: true,
 });
-console.log(2);
 fastify.register(dbConnector);
-console.log(3);
 fastify.register(firstRoute);
-console.log(4);
 
 fastify.listen({ port: 3000 }, function (err, address) {
   if (err) {
