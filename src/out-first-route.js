76e6f2ae92837b74818c706a9cbf8c028ed2944e
17Tralfagar <rybckin.rae2013@yandex.ru>
2022-11-14 13:00:30 +0700

    add a new route for replacement of bd

--- src/out-first-route.js
+++ src/out-first-route.js
@@ -21,6 +21,20 @@ async function routes(fastify, options) {
     return result;
   });
 
+  fastify.put("/animals/:animal", async (request, reply) => {
+    // в пример было app.put, но у нас же другой путь и я заменил на fastify.put
+    const result = await request.params.animal;
+    const details = { _id: new ObjectID(id) }; //как я понял, это перменная вводит новую id, тут немного не уверен
+    const note = { animal: request.body.body, title: request.body.title }; // вот эти две строчки 27 и 28 мне непонятны, я понимю, что этот код как раз таки создаёт замену, но в том примере, где я смотрел, данные считывались по ключ:значение, а у меня в свободной форме, не знаю как сделать в моём примере,help
+    db.collection("test_collection").update(details, note, (err, result) => {
+      if (!result) {
+        throw new Error("An error has occurred");
+      } else {
+        return result;
+      }
+    });
+  });
+
   const animalBodyJsonSchema = {
     type: "object",
     required: ["animal"],
