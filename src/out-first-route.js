0489f96f980785eb518a4752c931a31305c57d78
17Tralfagar <rybckin.rae2013@yandex.ru>
2022-11-14 15:42:56 +0700

    fix a new route

--- src/out-first-route.js
+++ src/out-first-route.js
@@ -22,11 +22,10 @@ async function routes(fastify, options) {
   });
 
   fastify.put("/animals/:animal", async (request, reply) => {
-    // в пример было app.put, но у нас же другой путь и я заменил на fastify.put
-    const result = await request.params.animal;
-    const details = { _id: new ObjectID(id) }; //как я понял, это перменная вводит новую id, тут немного не уверен
-    const note = { animal: request.body.body, title: request.body.title }; // вот эти две строчки 27 и 28 мне непонятны, я понимю, что этот код как раз таки создаёт замену, но в том примере, где я смотрел, данные считывались по ключ:значение, а у меня в свободной форме, не знаю как сделать в моём примере,help
-    db.collection("test_collection").update(details, note, (err, result) => {
+    const result = await collection.findOne({ animal: request.params.animal }); // я установил расширение, для id
+    const id = { _id: new ObjectID(id) }; // я понял что мне нужно и разобрал код, то есть 25 строка запрос и нахождение записи, 26 замена id, 27 замена значения
+    const animal = { animal: request.params.animal }; // в этой строчке я не уверен, ибо она у меня самая проблемная, я понимаю, что в ней надо написать такой код, который будет менять значение, но с трудом понимаю как, хотя чекнул прошлый роут и там пот такой схеме делал
+    db.collection("test_collection").updateOne(id, animal, (request, reply) => {
       if (!result) {
         throw new Error("An error has occurred");
       } else {
