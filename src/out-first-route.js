b9a3da71943b90fcc3509ab39597c7764e482d53
17Tralfagar <rybckin.rae2013@yandex.ru>
2022-11-15 13:00:35 +0700

    A new route completed

--- src/out-first-route.js
+++ src/out-first-route.js
@@ -1,44 +1,41 @@
-async function routes(fastify, options) {
-  const collection = fastify.mongo.db.collection("test_collection");
+const ObjectId = require('@fastify/mongodb').ObjectId;
 
-  fastify.get("/", async (request, reply) => {
-    return { hello: "world" };
+async function routes(fastify, options) {
+  const collection = fastify.mongo.db.collection('test_collection');
+
+  fastify.get('/', async (request, reply) => {
+    return { hello: 'world' };
   });
 
-  fastify.get("/animals", async (request, reply) => {
+  fastify.get('/animals', async (request, reply) => {
     const result = await collection.find().toArray();
     if (result.length === 0) {
-      throw new Error("No documents found");
+      throw new Error('No documents found');
     }
     return result;
   });
 
-  fastify.get("/animals/:animal", async (request, reply) => {
+  fastify.get('/animals/:animal', async (request, reply) => {
     const result = await collection.findOne({ animal: request.params.animal });
     if (!result) {
-      throw new Error("Invalid value");
+      throw new Error('Invalid value');
     }
     return result;
   });
 
-  fastify.put("/animals/:animal", async (request, reply) => {
-    const result = await collection.findOne({ animal: request.params.animal }); // я установил расширение, для id
-    const id = { _id: new ObjectID(id) }; // я понял что мне нужно и разобрал код, то есть 25 строка запрос и нахождение записи, 26 замена id, 27 замена значения
-    const animal = { animal: request.params.animal }; // в этой строчке я не уверен, ибо она у меня самая проблемная, я понимаю, что в ней надо написать такой код, который будет менять значение, но с трудом понимаю как, хотя чекнул прошлый роут и там пот такой схеме делал
-    db.collection("test_collection").updateOne(id, animal, (request, reply) => {
-      if (!result) {
-        throw new Error("An error has occurred");
-      } else {
-        return result;
-      }
-    });
+  fastify.put('/animals/:id', async (request, reply) => {
+    const id = new ObjectId(request.params.id);
+    const query = { _id: id };
+    const update = { $set: { animal: request.body.animal } };
+    const result = await collection.updateOne(query, update);
+    return result;
   });
 
   const animalBodyJsonSchema = {
-    type: "object",
-    required: ["animal"],
+    type: 'object',
+    required: ['animal'],
     properties: {
-      animal: { type: "string" },
+      animal: { type: 'string' },
     },
   };
 
@@ -46,7 +43,7 @@ async function routes(fastify, options) {
     body: animalBodyJsonSchema,
   };
 
-  fastify.post("/animals", { schema }, async (request, reply) => {
+  fastify.post('/animals', { schema }, async (request, reply) => {
     const result = await collection.insertOne({ animal: request.body.animal });
     return result;
   });
