90511d84417415acad28b64500f0aa647c195234
17Tralfagar <rybckin.rae2013@yandex.ru>
2022-11-17 15:10:12 +0700

    add a authorization

--- src/out-first-route.js
+++ src/out-first-route.js
@@ -2,6 +2,7 @@ const ObjectId = require('@fastify/mongodb').ObjectId;
 
 async function routes(fastify, options) {
   const collection = fastify.mongo.db.collection('test_collection');
+  const collectionUser = fastify.mongo.db.collection('users');
 
   fastify.get('/', async (request, reply) => {
     return { hello: 'world' };
@@ -40,6 +41,23 @@ async function routes(fastify, options) {
     }
     return result;
   });
+  //
+  fastify.get('/users', async (request, reply) => {
+    const result = await collectionUser.find().toArray();
+    if (result.length === 0) {
+      throw new Error('No documents found');
+    }
+    return result;
+  });
+
+  fastify.get('/users/:user', async (request, reply) => {
+    const result = await collectionUser.findOne({ user: request.params.user });
+    if (!result) {
+      throw new Error('Invalid value');
+    }
+    return result;
+  });
+  //
 
   const animalBodyJsonSchema = {
     type: 'object',
@@ -55,6 +73,27 @@ async function routes(fastify, options) {
 
   fastify.post('/animals', { schema }, async (request, reply) => {
     const result = await collection.insertOne({ animal: request.body.animal });
+    return result;
+  });
+  ///
+  const userBodyJsonSchema = {
+    type: 'object',
+    required_email: ['email'],
+    required_password: ['password'],
+    properties_email: {
+      email: { type: 'string' },
+    },
+    properties_password: {
+      password: { type: 'string' },
+    },
+  };
+
+  const schema1 = {
+    body: userBodyJsonSchema,
+  };
+
+  fastify.post('/user', { schema1 }, async (request, reply) => {
+    const result = await collectionUser.insertOne({ email: request.body.email, password: request.body.password });
     return result;
   });
 }
