0aaf0e8eb418a90a5f752517aec90be586307768
17Tralfagar <rybckin.rae2013@yandex.ru>
2022-11-28 14:38:29 +0700

    new project architecture

---
+++ src/animals-routes.js
@@ -0,0 +1,63 @@
+const ObjectId = require('@fastify/mongodb').ObjectId;
+const { auth } = require('./JWT-token');
+
+async function routesAnimals(fastify, options) {
+  const collection = fastify.mongo.db.collection('test_collection');
+
+  const animalBodyJsonSchema = {
+    type: 'object',
+    required: ['animal'],
+    properties: {
+      animal: { type: 'string' },
+    },
+  };
+
+  const schema = {
+    body: animalBodyJsonSchema,
+  };
+
+  fastify.get('/animals', async (request, reply) => {
+    auth(request, reply);
+    const result = await collection.find().toArray();
+    if (result.length === 0) {
+      throw new Error('No documents found');
+    }
+    return result;
+  });
+
+  fastify.get('/animals/:animal', async (request, reply) => {
+    auth(request, reply);
+    const result = await collection.findOne({ animal: request.params.animal });
+    if (!result) {
+      throw new Error('Invalid value');
+    }
+    return result;
+  });
+
+  fastify.put('/animals/:id', async (request, reply) => {
+    auth(request, reply);
+    const id = new ObjectId(request.params.id);
+    const result = await collection.updateOne({ _id: id }, { $set: { animal: request.body.animal } });
+    if (!result) {
+      throw new Error('Cannot access id');
+    }
+    return result;
+  });
+
+  fastify.delete('/animals/:id', async (request, reply) => {
+    auth(request, reply);
+    const id = new ObjectId(request.params.id);
+    const result = await collection.deleteOne({ _id: id }, { $lt: { animal: request.body.animal } });
+    if (!result) {
+      throw new Error('Cannot access id');
+    }
+    return result;
+  });
+
+  fastify.post('/animals', { schema }, async (request, reply) => {
+    auth(request, reply);
+    const result = await collection.insertOne({ animal: request.body.animal });
+    return result;
+  });
+}
+module.exports = { routesAnimals };
